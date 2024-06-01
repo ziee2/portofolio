1d0eefd8dc80306a3b148e31d54da123af6d3c91
unknown <fariedfarizi24@gmail.com>
2024-06-01 17:46:10 +0700

    feat(UI dan isi) : add new portofolio

---
+++ main.js
@@ -0,0 +1,32 @@
+let notLoadAll = document.querySelectorAll(".notload")
+
+
+var typed= new Typed(".text",{
+    strings:["Programmer"],
+    // strings:["AI Specialist", "Machine Learning Developer", "AI Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+window.onload = () => {
+	setTimeout(function() {
+		for (let i = 0; i < notLoadAll.length; i++) {
+			if (typeof notLoadAll[i].attributes["notload-delay"] !== "object") {
+				notLoadAll[i].classList.remove("notload")
+			} else if (typeof notLoadAll[i].attributes["notload-delay"] == "object") {
+				setTimeout(() => {
+					notLoadAll[i].classList.remove("notload")
+				}, 500)
+			}
+		}
+		loading.classList.add("off")
+		setTimeout(function() {
+			loading.classList.add("transparent")
+			setTimeout(function() {
+				loading.style.display = "none"
+			}, 1000);
+		}, 100);
+	}, 1000);
+}
